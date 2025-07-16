8d73d1aea2eb37f1744f878939fdacb3cf17cc25
Keshav <Keshav@LAPTOP-IDN3021A>
2025-07-16 20:25:05 +0400

    Add project files.

---
+++ NzWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NzWalks.API.Data;
+
+namespace NzWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NzWalksDbContext dbContext;
+
+        public RegionsController(NzWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}
---
+++ NzWalks.API/Model/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NzWalks.API.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegiondId { get; set; }
+
+        //navigation property
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
