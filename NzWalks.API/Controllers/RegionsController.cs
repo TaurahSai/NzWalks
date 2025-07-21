29861d09292a07619bb734043f59707be9af67d1
Keshav <Keshav@LAPTOP-IDN3021A>
2025-07-21 18:25:08 +0400

    change functions to async version

--- NzWalks.API/Controllers/RegionsController.cs
+++ NzWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NzWalks.API.Data;
 
 namespace NzWalks.API.Controllers
@@ -14,11 +15,10 @@ namespace NzWalks.API.Controllers
             this.dbContext = dbContext;
         }
 
-
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var regions = dbContext.Regions.ToList();
+            var regions = await dbContext.Regions.ToListAsync();
 
             var regionsDto = new List<Model.DTO.RegionDto>();
 
@@ -37,9 +37,9 @@ namespace NzWalks.API.Controllers
 
         [HttpGet]
         [Route("{id:Guid}")]
-        public IActionResult GetById([FromRoute] Guid id)
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
-            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            var region = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id); // Use FirstOrDefaultAsync instead of FirstOrDefault
 
             if (region == null)
             {
@@ -54,6 +54,80 @@ namespace NzWalks.API.Controllers
                 RegionImageUrl = region.RegionImageUrl
             };
 
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Model.DTO.AddRegionRequestDto addRegionRequestDto)
+        {
+            if (addRegionRequestDto == null)
+            {
+                return BadRequest("Region data is required.");
+            }
+            var regionDomainModel = new Model.Domain.Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+            await dbContext.Regions.AddAsync(regionDomainModel); 
+            await dbContext.SaveChangesAsync();
+
+            var regionDto = new Model.DTO.RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] Model.DTO.UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            if (updateRegionRequestDto == null)
+            {
+                return BadRequest("Region data is required.");
+            }
+            var region = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            region.Code = updateRegionRequestDto.Code;
+            region.Name = updateRegionRequestDto.Name; 
+            region.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+            await dbContext.SaveChangesAsync();
+            var regionDto = new Model.DTO.RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var region = await dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            dbContext.Regions.Remove(region);
+            await dbContext.SaveChangesAsync();
+            var regionDto = new Model.DTO.RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
             return Ok(regionDto);
         }
     }
